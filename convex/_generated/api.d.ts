6d10fa8a7c0ed0ff8219aaa0ba1c2c5815581621
AdamAmr05 <adamamr@icloud.com>
2026-02-22 22:59:44 +0200

    Regenerate Convex API types after schema update

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -11,8 +11,14 @@
 import type * as auth from "../auth.js";
 import type * as lib_auth from "../lib/auth.js";
 import type * as lib_env from "../lib/env.js";
+import type * as lib_ticketValidators from "../lib/ticketValidators.js";
+import type * as lib_tickets from "../lib/tickets.js";
 import type * as lib_validators from "../lib/validators.js";
 import type * as resolverRequests from "../resolverRequests.js";
+import type * as ticketsManager from "../ticketsManager.js";
+import type * as ticketsReporter from "../ticketsReporter.js";
+import type * as ticketsResolver from "../ticketsResolver.js";
+import type * as ticketsShared from "../ticketsShared.js";
 
 import type {
   ApiFromModules,
@@ -24,8 +30,14 @@ declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   "lib/auth": typeof lib_auth;
   "lib/env": typeof lib_env;
+  "lib/ticketValidators": typeof lib_ticketValidators;
+  "lib/tickets": typeof lib_tickets;
   "lib/validators": typeof lib_validators;
   resolverRequests: typeof resolverRequests;
+  ticketsManager: typeof ticketsManager;
+  ticketsReporter: typeof ticketsReporter;
+  ticketsResolver: typeof ticketsResolver;
+  ticketsShared: typeof ticketsShared;
 }>;
 
 /**
